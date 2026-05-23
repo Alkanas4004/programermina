6e94c0a4be9c1466ddf61ff05468a522582df4de
Alkanas4004 <thestormleaksgang555@gmail.com>
2026-05-24 00:10:54 +0300

    Add application constants and project categories

---
+++ js/config/constants.js
@@ -0,0 +1,25 @@
+/**
+ * Application Constants
+ */
+
+const APP_CONFIG = {
+    NAME: 'MINA Portfolio',
+    VERSION: '3.0.0',
+    AUTHOR: 'Mina Programmer',
+    EMAIL: 'mina@programmer.dev',
+    SOCIAL: {
+        github: 'https://github.com/mina',
+        linkedin: 'https://linkedin.com/in/mina',
+        twitter: 'https://twitter.com/mina'
+    }
+};
+
+const PROJECT_CATEGORIES = {
+    WEB: 'web',
+    MOBILE: 'mobile',
+    SECURITY: 'security',
+    AI: 'ai'
+};
+
+window.APP_CONFIG = APP_CONFIG;
+window.PROJECT_CATEGORIES = PROJECT_CATEGORIES;
